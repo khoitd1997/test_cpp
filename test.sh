f8ad1b6257f6e2b28c7fd25e0f45de2491c94689
khoitd1997 <khoidinhtrinh@gmail.com>
2019-05-20 17:33:14 -0700

    add test and build

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-set -e
-
 currDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-mkdir -p ${currDir}/build
 
 cd ${currDir}/build
-cmake -G Ninja .. && ninja 
+GTEST_COLOR=1 ctest --verbose
