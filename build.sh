76cd8fed6db3e1313834a811e6602b28107c9057
khoitd1997 <khoidinhtrinh@gmail.com>
2019-07-03 21:22:47 -0700

    resolve conflict

--- build.sh
+++ build.sh
@@ -6,11 +6,7 @@ currDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p ${currDir}/build
 
 cd ${currDir}/build
-<<<<<<< HEAD
-cmake -G Ninja .. && ninja 
+cmake -G Ninja .. && ninja
 cd bin
 ./test_cpp
 # GTEST_COLOR=1 ctest --verbose
-=======
-cmake -G Ninja .. && ninja 
->>>>>>> 39b3852d70e08a10293e88a1eeb97c1e73e8102e
